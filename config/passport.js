7d929807659406a6041ea8bd607045a17dd060fe
guangpnt <70946141+guangpnt@users.noreply.github.com>
2021-01-23 14:23:42 +0700

    Edit passport

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,8 @@ module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/google/callback'
+        callbackURL: '/google/callback',
+        proxy: true 
     },
     async (accessToken, refreshToken, profile, done) => {   
         const newGUser = {
